8bcc309773ee3decd38022a53d9061553e1a0141
sidgawas <108471891+sidgawas@users.noreply.github.com>
2026-05-10 04:06:03 +0530

    Bootstraps modeller service application

---
+++ dmnex-modeller-service/src/main/java/com/github/sidgawas/dmnex/modeller_service/DmnexModellerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.sidgawas.dmnex.modeller_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmnexModellerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmnexModellerServiceApplication.class, args);
+	}
+
+}
---
+++ dmnex-modeller-service/src/test/java/com/github/sidgawas/dmnex/modeller_service/DmnexModellerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sidgawas.dmnex.modeller_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmnexModellerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
